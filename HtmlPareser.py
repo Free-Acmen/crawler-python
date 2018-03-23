76f8a63d9842f3f14a6047e6b21f22839db328e7
Free-Acmen <lmllsq@126.com>
2018-03-23 17:30:14 +0800

    init

---
+++ HtmlPareser.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# name HtmlParser.py
+
+class HtmlParser(object):
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
+    
---
+++ UrlManager.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# name UrlManager.py
+
+class UrlManager(object):
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
+    
